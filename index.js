613e21e0f388d122f2d3c546cf5d57a17298027e
MunNX <thaiboomnguyen@gmail.com>
2024-01-06 01:59:49 +0700

    update cors v6

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ mongoose.connection.on('disconnected', () => {
 app.use(cors({
     origin: ['http://localhost:3000', 'http://localhost:3001', 'https://qr-checkin-theta.vercel.app',
         'https://qrcodectyaq-8e31be181e4d.herokuapp.com', 'https://qr-checkin-cty-a-quang.vercel.app',
-        'qxotttabcxyz.online', 'www.qxotttabcxyz.online', 'https://www.qxotttabcxyz.online'],
+        'qxotttabcxyz.online', 'www.qxotttabcxyz.online', 'https://www.qxotttabcxyz.online',
+        'qr-checkin-theta.vercel.app'],
     credentials: true,
 }));
 // app.options('*', cors());
